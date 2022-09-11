25520c2d5ab190f26a9f0216ac8f64baa886a49c
adrian7cd <adrianipsys@proton.me>
2022-09-11 21:18:37 +0200

    c

--- script.js
+++ script.js
@@ -8,3 +8,29 @@ const button6 = document.getElementById("button6");
 const button7 = document.getElementById("button7");
 const button8 = document.getElementById("button8");
 const button9 = document.getElementById("button9");
+
+let board = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+let isGameActive = true;
+
+const X_WON = "X_WON";
+const O_WON = "O_WON";
+const TIE = "TIE";
+
+/* 
+  Board fields
+  [0] [1] [2]
+  [3] [4] [5]
+  [6] [7] [8]
+*/
+
+const winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
