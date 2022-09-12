18c170d331085057d9170ce126c9acdbbec9bab0
adrian7cd <adrianipsys@proton.me>
2022-09-12 22:35:08 +0200

    added game functionality

--- script.js
+++ script.js
@@ -1,36 +1,124 @@
-// Buttons
-const button1 = document.getElementById("button1");
-const button2 = document.getElementById("button2");
-const button3 = document.getElementById("button3");
-const button4 = document.getElementById("button4");
-const button5 = document.getElementById("button5");
-const button6 = document.getElementById("button6");
-const button7 = document.getElementById("button7");
-const button8 = document.getElementById("button8");
-const button9 = document.getElementById("button9");
+window.addEventListener("DOMContentLoaded", () => {
+  // Buttons
+  const tiles = Array.from(document.querySelectorAll(".tile"));
+  const playerDisplay = document.querySelector(".display-player");
+  const announcer = document.querySelector("#announcer");
+  const reset = document.querySelector("#reset");
 
-let board = ["", "", "", "", "", "", "", "", ""];
-let currentPlayer = "X";
-let isGameActive = true;
+  let board = ["", "", "", "", "", "", "", "", ""];
+  let currentPlayer = "X";
+  let isGameActive = true;
+  let roundWon;
 
-const X_WON = "X_WON";
-const O_WON = "O_WON";
-const TIE = "TIE";
+  const X_WON = "X_WON";
+  const O_WON = "O_WON";
+  const TIE = "TIE";
 
-/* 
+  /* 
   Board fields
   [0] [1] [2]
   [3] [4] [5]
   [6] [7] [8]
 */
 
-const winningConditions = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
+  const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  tiles.forEach((tile, index) => {
+    tile.addEventListener("click", () => userAction(tile, index));
+  });
+
+  function handleResultValidation() {
+    let roundWon = false;
+    for (let i = 0; i <= 7; i++) {
+      const winConditions = winningConditions[i];
+      const a = board[winConditions[0]];
+      const b = board[winConditions[1]];
+      const c = board[winConditions[2]];
+      if (a === "" || b === "" || c === "") {
+        continue;
+      }
+      if (a === b && b === c) {
+        roundWon = true;
+        break;
+      }
+    }
+  }
+
+  if (roundWon) {
+    announce(currentPlayer === "X" ? X_WON : O_WON);
+    isGameActive = false;
+    return;
+  }
+
+  if (!board.includes("")) {
+    announce(TIE);
+  }
+
+  const announce = (type) => {
+    switch (type) {
+      case X_WON:
+        announcer.innerHTML = 'Player <span class="playerO">O</span> won';
+        break;
+      case O_WON:
+        announcer.innerHTML = 'Player <span class="playerX">X</span> won';
+        break;
+      case TIE:
+        announcer.innerHTML = "Tie";
+        break;
+    }
+  };
+
+  const isValidAction = (tile) => {
+    if (tile.innerText === "X" || tile.innerText === "O") {
+      return false;
+    }
+    return true;
+  };
+
+  const updateBoard = (index) => {
+    board[index] = currentPlayer;
+  };
+
+  const changePlayer = () => {
+    playerDisplay.classList.remove(`player${currentPlayer}`);
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    playerDisplay.innerText = currentPlayer;
+    playerDisplay.classList.add(`player${currentPlayer}`);
+  };
+
+  const userAction = (tile, index) => {
+    if (isValidAction(tile) && isGameActive) {
+      tile.innerText = currentPlayer;
+      tile.classList.add(`player${currentPlayer}`);
+      updateBoard(index);
+      handleResultValidation();
+      changePlayer();
+    }
+  };
+
+  const resetBoard = () => {
+    board = ["", "", "", "", "", "", "", "", ""];
+    isGameActive = true;
+    announcer.classList.add("hide");
+
+    if (currentPlayer === "O") {
+      changePlayer();
+    }
+
+    tiles.forEach((tile) => {
+      tile.innerText = "";
+      tile.classList.remove("playerX");
+      tile.classList.remove("playerO");
+    });
+  };
+  reset.addEventListener("click", resetBoard);
+});
